0ae7799e0c855a9454f7f58dea6521d9b6c994e2
Shuai Shao <shaoshuai321@gmail.com>
2013-09-16 17:28:49 +0800

    maximum openned files

--- scrawler.js
+++ scrawler.js
@@ -5,23 +5,36 @@ var fs = require('fs');
 var child_process = require('child_process');
 var command = 'wget -E -H -p -nd -nH -e robots=off -Ptop500/%dir %domain';
 var failed = [];
+var MAX_SESSION = 50;
+var sites;
+
+function get(domain) {
+    var cmd = command.replace('%dir', domain).replace('%domain', domain);
+    console.log('executing ' + cmd);
+    var child = child_process.exec(cmd, function (error, stdout, stderr) {
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+            failed.push(domain);
+        }
+
+        if (sites.length != 0)
+            get(sites.pop());
+    });
+}
 
 fs.readFile(input, 'utf8', function (err, data) {
     if (err) {
         console.log('Invalid input');
     } else {
-        var sites = data.split(/\r?\n/);
-        sites.forEach(function (domain) {
-            var cmd = command.replace('%dir', domain).replace('%domain', domain);
-            console.log('executing ' + cmd);
-            var child = child_process.exec(cmd, function (error, stdout, stderr) {
-                  console.log('stderr: ' + stderr);
-                  if (error !== null) {
-                      console.log('exec error: ' + error);
-                      failed.push(domain);
-                  }
-            });
-        });
+        sites = data.split(/\r?\n/);
+        var sessions = 0;
+        while (sessions < MAX_SESSION) {
+            sessions++;
+            var domain = sites.pop();
+            if (domain)
+                get(domain);
+        }
     }
 });
 
