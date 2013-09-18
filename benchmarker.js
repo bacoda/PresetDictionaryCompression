cd7734cedb1294c4088637e7dcb61a5a26de2247
Shuai Shao <shaoshuai321@gmail.com>
2013-09-18 10:39:29 +0800

    fixes

--- benchmarker.js
+++ benchmarker.js
@@ -14,14 +14,11 @@ console.log('Benchmarking with dictionary: ' + keyword + ' in ' + directory);
 
 child_process.exec(encoder.replace('%path', keyword), function (error, stdout, stderr) {
     var zipped = keyword + '.7z';
-    console.log(zipped);
-    console.log(fs.statSync(zipped));
-
-    keyword_size = util.inspect(fs.statSync(zipped)).size;
+    keyword_size = fs.statSync(zipped).size;
     console.log('Dictionary compressed size:' + keyword_size);
 
     require('findit')(directory).on('file', function (file, stat) {
-        console.log(file);
+        //console.log(file);
     });
 
 });
