31cd3610a6cf606392331443ed55dbb7c65cb8f9
Shuai Shao <shaoshuai321@gmail.com>
2013-09-24 18:39:36 +0800

    fix

--- benchmarker.js
+++ benchmarker.js
@@ -13,7 +13,9 @@ var keyword = command_line.dict || 'keyword.txt';
 
 var base_gzip, lzma, lzma_dict, gzip_dict, zopfli;
 
-var current_gzip_size = 0, dict_lzma_size, dict_gzip_size;
+var current_gzip_textual_size = 0;  // gzip size of all compressible files(html/css/js) processed so far
+var current_gzip_other_size = 0;    // gzip size of all other files processed so far
+var dict_lzma_size, dict_gzip_size;
 var rebase = false;
 var output = [];
 var intervalId;
@@ -68,7 +70,7 @@ function appendOutput(obj) {
     fs.appendFileSync(output_file, str);
 }
 
-function testSite(directory, callback) {
+function testPage(directory, callback) {
     var base = PATH.resolve(input_folder);
     var current = PATH.resolve(directory);
     var relative = PATH.relative(base, current);
@@ -77,168 +79,221 @@ function testSite(directory, callback) {
         return;
     }
 
-    if (command_line.verbose) {
-        console.log('Parsing webpage:' + directory);
-    }
-
+    var stats = tasks.getTotals();
+    console.log('Parsing webpage ' + stats.completed + '/' + stats.total + ':' + directory);
+    
     var name = PATH.basename(directory);
-
+    if (base_gzip.pages[name]) {
+        console.error('Error: Ignoring duplicate page folder: ' + name);
+        callback();
+        return;
+    }
+    
+    base_gzip.pages[name] = {
+        files:{},
+        stats:{
+          total:0,
+          other:0,
+          html:0,
+          css:0,
+          js:0
+        }
+    };
+    
+    var files_task = new taskgroup().setConfig({concurrency: 16});
+  
     var fileFinder = finder(directory);
     fileFinder.on('file', function (file, stat) {
         //if (command_line.verbose) {
         //    console.log('Adding file:' + file);
         //}
 
-        tasks.addTask(function (completion) {
+        files_task.addTask(function (completion) {
             var type = fileExt(file);
+            var file_name = PATH.basename(path);
+            var page_info = base_gzip.pages[page_name];
+
+            if (!page_info[file_name])
+                page_info[file_name] = {
+                    type:type,
+                    size:0
+                };
+                
+            var file_info = page_info[file_name]
+            
             if (type == '.css' || type == '.html' || type == '.js') {
-                benchmarkFile(name, file, type, completion);
+                benchmarkFile(file_info, file, completion);
+                
+                if (rebase) {
+                    if (type == '.css')
+                        page_info.stats.css += file_info.size;
+                    else if (type == '.html')
+                        page_info.stats.html += file_info.size;
+                    else
+                        page_info.stats.js += file_info.size;
+                }
             } else {
                 if (type != '.7z' && type != '.dict' && type != '.gz') {
+                    if (rebase) {
+                        file_info.size = fs.statSync(file).size;
+                        page_info.stats.other += file_info.size;
+                    }
                 }
                 completion();
             }
         });
     });
 
-    fileFinder.on('end', callback);
+    fileFinder.on('end', function() {
+       files_task.run();
+       files_task.once('complete', function() {
+          current_gzip_textual_size += page_info.stats.html + page_info.stats.css + page_info.stats.js;
+          current_gzip_other_size += page_info.stats.other;
+          
+          dumpPage(name);
+          dumpTotal();
+          callback();
+       });
+    });
 }
 
-function benchmarkFile(site, path, type, completion) {
-  var gzip_size;
-
+function benchmarkFile(file_info, path, completion) {
     var cat_file = path + '.dict';
     var command = 'cat "' + keyword + '" "' + path + '" > "' + cat_file + '"';
-    
-  var localTask = new taskgroup();
-  
-  if (command_line.verbose) {
-      console.log('Testing file:' + path);
-  }
-
-    // gzip
-  if (rebase) {
-      localTask.addTask(function (callback) {
-          gzip(path, function () {
-              gzip_size = fs.statSync(path + '.gz').size;
-              base_gzip.total += gzip_size;
-              base_gzip.sizes[path] = gzip_size;
-
-              if (command_line.verbose) {
-                  console.log('Gzip size:' + gzip_size);
-              }
-              callback();
-          });
-      });
-  } else {
-      gzip_size = base_gzip.sizes[path];
-      if (!gzip_size) {
-          console.error('Failed to get size for ' + path);
-          process.exit(1);
-      }
-  }
-  
-    // cat
-  if (lzma_dict || gzip_dict) {
-    localTask.addTask(function (callback) {
-        child_process.exec(command, function (error, stdout, stderr) {
-            if (error) {
-                console.error(error);
-                console.error('when executing command: ' + command);
-                process.exit(1);
-            }
-            callback();
-        });
-    });
-  }
-    
-    // 7zip
-  if (lzma) {
-    localTask.addTask(function (callback) {
-        zip(path, function () {
-            var zipped = path + '.7z';
-            var size_7z = fs.statSync(zipped).size;
-            if (size_7z > gzip_size)
-                size_7z = gzip_size;
-            lzma.total += size_7z;
-            callback();
-        });
-    });
-  }
       
-  // 7zip with dict
-  if (lzma_dict) {
-    localTask.addTask(function (callback) {
-        zip(cat_file, function () {
-            var cat_size = fs.statSync(cat_file + '.7z').size - dict_lzma_size + 90;
+    var localTask = new taskgroup();
+    
+    if (command_line.verbose) {
+        console.log('Testing file:' + path);
+    }
 
-            if (cat_size > gzip_size) {
-                console.log('Unexpected growth in size. ' + gzip_size + '->' + cat_size + ' ' + path);
-                cat_size = gzip_size;
-            }
-
-            lzma_dict.total += cat_size;
-            callback();
+      // gzip
+    if (rebase) {
+        localTask.addTask(function (callback) {
+            gzip(path, function () {
+                file_info.size = fs.statSync(path + '.gz').size;
+                callback();
+            });
         });
-    });
-  }
-  
-  if (gzip_dict) {
+    }
+    
+    if (!file_info.size) {
+        console.error('Failed to get size for ' + path);
+        process.exit(1);
+    }
+      
+      // cat
+    if (lzma_dict || gzip_dict) {
       localTask.addTask(function (callback) {
-          gzip(cat_file, function () {
-              var size = fs.statSync(cat_file + '.gz').size - dict_gzip_size;
-              if (size > gzip_size) {
-                  console.log('Unexpected growth in size. ' + gzip_size + '->' + size + ' ' + path);
-                  size = gzip_size;
+          child_process.exec(command, function (error, stdout, stderr) {
+              if (error) {
+                  console.error(error);
+                  console.error('when executing command: ' + command);
+                  process.exit(1);
               }
-              gzip_dict.total += size;
               callback();
           });
       });
-  }
-
-  if (zopfli) {
-    localTask.addTask(function (callback) {
-      gzip_zopfli(path, function(){
-        var size = fs.statSync(path + '.gz').size;
-        if (size > gzip_size) {
-          size = gzip_size;
-        }
-        zopfli.total += size;
-        callback();
+    }
+      
+      // 7zip
+    if (lzma) {
+      localTask.addTask(function (callback) {
+          zip(path, function () {
+              var zipped = path + '.7z';
+              var size_7z = fs.statSync(zipped).size;
+              if (size_7z > gzip_size)
+                  size_7z = gzip_size;
+              lzma.total += size_7z;
+              callback();
+          });
       });
-    });
-  }
+    }
+        
+    // 7zip with dict
+    if (lzma_dict) {
+      localTask.addTask(function (callback) {
+          zip(cat_file, function () {
+              var cat_size = fs.statSync(cat_file + '.7z').size - dict_lzma_size + 90;
 
-  localTask.once('complete', function() {
-    current_gzip_size += gzip_size;
-    completion();
-  });
-  localTask.run();
+              if (cat_size > gzip_size) {
+                  console.log('Unexpected growth in size. ' + gzip_size + '->' + cat_size + ' ' + path);
+                  cat_size = gzip_size;
+              }
+
+              lzma_dict.total += cat_size;
+              callback();
+          });
+      });
+    }
+    
+    if (gzip_dict) {
+        localTask.addTask(function (callback) {
+            gzip(cat_file, function () {
+                var size = fs.statSync(cat_file + '.gz').size - dict_gzip_size;
+                if (size > gzip_size) {
+                    console.log('Unexpected growth in size. ' + gzip_size + '->' + size + ' ' + path);
+                    size = gzip_size;
+                }
+                gzip_dict.total += size;
+                callback();
+            });
+        });
+    }
+
+    if (zopfli) {
+      localTask.addTask(function (callback) {
+        gzip_zopfli(path, function(){
+          var size = fs.statSync(path + '.gz').size;
+          if (size > gzip_size) {
+            size = gzip_size;
+          }
+          zopfli.total += size;
+          callback();
+        });
+      });
+    }
+
+    localTask.once('complete', function() {
+        completion();
+    });
+    localTask.run();
 }
 
 function percent(gzip, other) {
-  var result = (gzip-other)*100/gzip;
-  return result.toPrecision(3) + '%';
+    var result = (gzip-other)*100/gzip;
+    return result.toPrecision(3) + '%';
 }
 
-function dump() {
-    var stats = tasks.getTotals();
-    var str = 'Progress:' + (stats.completed*100/stats.total).toPrecision(3) + '% \tgzip:' + current_gzip_size;
+function dumpPage(name) {
+    //var str = 'gzip:' + current_gzip_size;
+}
 
-    if (gzip_dict) {
-        str += 'gzip(dict):' + gzip_dict.total + '('+ percent(current_gzip_size, gzip_dict.total) + ')';
+// for a single compression algorithm
+function dumpTotalScheme(obj, total) {
+    if (obj) {
+        var total_size = obj.total + (total ? current_gzip_other_size : 0);
+        var gzip_size = current_gzip_textual_size + (total ? current_gzip_other_size : 0);
+        return obj.type + ':\t' + total_size + '('+ percent(gzip_size, total_size) + ')\t';
     }
-    if (lzma) {
-        str += '\tlzma:' + lzma.total + '(' + percent(current_gzip_size, lzma.total) + ')';
-    }
-    if (lzma_dict) {
-        str +='\tlzma(dict):' + lzma_dict.total + '(' + percent(current_gzip_size, lzma_dict.total) + ')';
-    }
-    if (zopfli) {
-        str += '\tzopfli:' + zopfli.total + '(' + percent(current_gzip_size, zopfli.total) + ')';
-    }
+    return '';
+}
+
+function dumpTotal() {
+    var str = 'Overall stats(textual content) gzip:' + current_gzip_textual_size + '\t' +
+              dumpTotalScheme(gzip_dict) + 
+              dumpTotalScheme(lzma) +
+              dumpTotalScheme(lzma_dict) +
+              dumpTotalScheme(zopfli);
+
+    console.log(str);
     
+    var str = 'Overall stats                  gzip:' + current_gzip_textual_size + current_gzip_other_size +  '\t' +
+              dumpTotalScheme(gzip_dict, true) + 
+              dumpTotalScheme(lzma, true) +
+              dumpTotalScheme(lzma_dict, true) +
+              dumpTotalScheme(zopfli, true);
+
     console.log(str);
 }
 
@@ -251,15 +306,11 @@ function saveResult(obj) {
 }
 
 function done() {
-    dump();
-
-    clearInterval(intervalId);
-
     saveResult(base_gzip);
     saveResult(gzip_dict);
     saveResult(lzma);
     saveResult(lzma_dict);
-  saveResult(zopfli);
+    saveResult(zopfli);
 }
 
 console.log('Benchmarking with dictionary: ' + keyword + ' in ' + input_folder + ' output: ' + output);
@@ -270,9 +321,6 @@ if (!fs.existsSync(keyword)) {
 }
 
 var tasks = new taskgroup();
-tasks.setConfig({
-    concurrency: 30
-});
 
 zip(keyword, function () {
     var zipped = keyword + '.7z';
@@ -296,7 +344,7 @@ zip(keyword, function () {
             base_gzip = {
                 total: 0,
                 type: 'gzip',
-                sizes: {},
+                pages: {},
             };
         }
 
@@ -333,20 +381,18 @@ zip(keyword, function () {
         var siteFinder = finder(input_folder);
         siteFinder.on('directory', function (path, stat) {
             tasks.addTask(function (completion) {
-                testSite(path, completion);
+                testPage(path, completion);
             });
         });
 
         siteFinder.on('end', function () {
             tasks.once('complete', function () {
-                console.log('==============done!!!!!!!!!!!!!!!!');
+                console.log('==============done=============');
                 done();
             });
 
             console.log('Compression testing...');
             tasks.run();
         });
-
-        intervalId = setInterval(dump, 2000);
     });
 });
